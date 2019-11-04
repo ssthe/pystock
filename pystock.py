67279c4120c492f6f40f71ede59d7cce2485d051
ssthe <ssyscott@gmail.com>
2019-11-04 10:56:18 +0800

    Added pop window for graphs

--- pystock.py
+++ pystock.py
@@ -6,6 +6,11 @@ import pandas as pd
 
 from tkinter import Tk
 from tkinter import ttk
+from tkinter import Toplevel
+
+import matplotlib as mp
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 
 
 ###############
@@ -25,26 +30,26 @@ func_list = None
 #######################
 # COMPUTATIAONAL PART #
 #######################
-	
+
 # variable substitution and eqation evaluation
 def sub(eqa, var_df):
 
-	# helper method for function sub
-	# TODO: Add zero division detection and handling
-	def sub_helper(map, eqa):
-		return eval(eqa, map)
+    # helper method for function sub
+    # TODO: Add zero division detection and handling
+    def sub_helper(map, eqa):
+        return eval(eqa, map)
 
-	return var_df.apply(sub_helper, axis = 1, args = eqa)
+    return var_df.apply(sub_helper, axis=1, args=eqa)
 
 # validate equation
 def eqation_validate(eqa, vars):
-	try:
-		eval(eqa, dict(vars, [1]*len(vars)))
-	except ZeroDivisionError:
-		pass
-	except:
-		return False
-	return True
+    try:
+        eval(eqa, dict(vars, [1]*len(vars)))
+    except ZeroDivisionError:
+        pass
+    except:
+        return False
+    return True
 
 
 #################
@@ -53,60 +58,60 @@ def eqation_validate(eqa, vars):
 
 # initialize data
 def data_load():
-	pt.Path(floc).mkdir(exist_ok = True)
-	global var_list
-	global func_list
-	var_list = read_persist(floc_vars)
-	func_list = read_persist(floc_funcs)
+    pt.Path(floc).mkdir(exist_ok=True)
+    global var_list
+    global func_list
+    var_list = read_persist(floc_vars)
+    func_list = read_persist(floc_funcs)
 
 # add new variable
 def add_var(name, func_and_args, csv_loc):
-	var_list.append(pd.DataFrame([name, str(func_and_args), csv_loc], 
-		columns=["name", "func-and-args", "csv-location"]), ignore_index=True)
+    var_list.append(pd.DataFrame([name, str(func_and_args), csv_loc],
+                        columns=["name", "func-and-args", "csv-location"]), ignore_index=True)
 
 # add new function
 def add_func(name, func, var_map):
-	func_list.append(pd.DataFrame([name, str(func), str(var_map)], 
-		columns=["name", "value", "var-map"]), ignore_index=True)
+    func_list.append(pd.DataFrame([name, str(func), str(var_map)],
+                        columns=["name", "value", "var-map"]), ignore_index=True)
 
 # write to disk
 def write_all():
-	write_var()
-	write_func()
+    write_var()
+    write_func()
 
 # write variables
 def write_var():
-	var_list.to_csv(floc_vars)
+    var_list.to_csv(floc_vars)
 
 # write function
 def write_func():
-	func_list.to_csv(floc_funcs)
-	
+    func_list.to_csv(floc_funcs)
+
 # create if not exist
 def read_persist(file_loc):
-	if pt.Path(file_loc).is_file():
-		result = pd.read_csv(file_loc)
-	else:
-		result = pd.DataFrame()
-		result.to_csv(floc_vars)
-	return result
+    if pt.Path(file_loc).is_file():
+        result = pd.read_csv(file_loc)
+    else:
+        result = pd.DataFrame()
+        result.to_csv(floc_vars)
+    return result
 
 # retrieve data from baostock
 def get_stock_data(func, args, kwargs):
-	return func(*args, **kwargs)
+    return func(*args, **kwargs)
 
 # return a dict of <function name, function>
 def get_all_func():
-	result = dict(sp.getmembers(bs, sp.isfunction))
-	if 'login' in result:
-		del result['login']
-	if 'logout' in result:
-		del result['logout']
-	return result
+    result = dict(sp.getmembers(bs, sp.isfunction))
+    if 'login' in result:
+        del result['login']
+    if 'logout' in result:
+        del result['logout']
+    return result
 
 # return a tuple of arg names and kwarg names (args, kwargs)
 def get_all_args(func):
-	return sp.signature(func).parameters
+    return sp.signature(func).parameters
 
 ############
 # GUI PART #
@@ -114,35 +119,51 @@ def get_all_args(func):
 
 # initialize gui
 def gui_load():
-	gui = Tk()
-	bao_list = get_all_func()
-	return gui
+    gui = Tk()
+    vf = var_frame(gui)
+    ff = func_frame(gui)
+    return gui
 
 # variable window
 def var_frame(master):
-	global var_list
-	f = ttk.Frame(master)
-	lf = list_frame(f, var_list)
-	lf.pack()
-	return f
+    global var_list
+    f = ttk.Frame(master)
+    lf = list_frame(f, var_list)
+    lf.pack()
+    return f
 
 # function window
 def func_frame(master):
-	global func_list
-	f = ttk.Frame(master)
-	lf = list_frame(f, func_list)
-	lf.pack()
-	return f
+    global func_list
+    f = ttk.Frame(master)
+    lf = list_frame(f, func_list)
+    lf.pack()
+    return f
 
 # universal list frame
 def list_frame(master, df):
-	tree = ttk.Treeview(master, columns=df.columns.values)
-	for col in df.columns.values:
-		tree.column(col, width=100)
-		tree.heading(col, text=col)
-	for row in len(df):
-		tree.insert('', row, values=(df.iloc[row].tolist))
-	return tree
+    tree = ttk.Treeview(master, columns=df.columns.values)
+    for col in df.columns.values:
+        tree.column(col, width=100)
+        tree.heading(col, text=col)
+    for row in len(df):
+        tree.insert('', row, values=(df.iloc[row].tolist))
+    return tree
+
+# calls a popup window for a plot
+def pop_graph(name, df):
+	pop = Toplevel()
+	lf = ttk.LabelFrame(pop, text=name)
+	lf.grid(row=0, column=0, sticky='nwes', padx=3, pady=3)
+
+	fig = Figure(figsize=(5,4), dpi=100)
+	ax = fig.add_subplot(111)
+
+	df.reset_index().plot(x='index', y=df.columns.values, ax=ax)
+	
+	canvas = FigureCanvasTkAgg(fig, master=lf)
+	canvas.draw()
+	canvas.get_tk_widget().grid(row=0, column=0)
 
 
 ###############
@@ -151,6 +172,6 @@ def list_frame(master, df):
 
 # Driver code
 if __name__ == "__main__":
-	data_load()
-	gui = gui_load()
-	gui.mainloop()
+    data_load()
+    gui = gui_load()
+    gui.mainloop()
